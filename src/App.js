57bfac56fd6bc98b670ab255a4f19e464048519f
Celdama <celdama.dev@gmail.com>
2022-02-10 18:16:40 +0100

    cofigured tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { TodoListStore } from './Components/TodoList';
+import './App.css';
 
 function App() {
   return (
     <div className='App'>
+      <h1 className='text-3xl font-bold underline'>Todo List with Redux</h1>
       <TodoListStore />
     </div>
   );
