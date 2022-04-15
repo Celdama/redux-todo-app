28643d75701e831d3c1ad6c79cdbb8fd6809ec1c
Celdama <celdama.dev@gmail.com>
2022-04-15 07:19:49 +0200

    Store/reducers/filterReducer: add eslint disable next line

--- src/Store/reducers/filterReducer.js
+++ src/Store/reducers/filterReducer.js
@@ -1,5 +1,6 @@
 export const FILTER_TODO_ACTION = 'FILTER_TODO_ACTION';
 
+// eslint-disable-next-line default-param-last
 export const filterReducer = (state = null, action) => {
   switch (action.type) {
     case FILTER_TODO_ACTION:
